e0964106abb305c9ac85a65e7285bd22a2ecb561
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-09-27 21:32:47 +0530

    clerk setup

---
+++ app/(app)/video-upload/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Video_Upload() {
+  return (
+    <div>Video_Upload</div>
+  )
+}
+
+export default Video_Upload
--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,8 @@ import { clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher([
-    "/signin",
-    "/signup",
+    "/sign-in",
+    "/sign-up",
     "/",
     "/home",
 ])
@@ -24,11 +24,11 @@ export default clerkMiddleware((auth, req)=>{
     //not logged in
     if(!userId){
         if(!isPublicRoute(req) && !isPublicApiRoute(req)){
-            return NextResponse.redirect(new URL("/signin", req.url));
+            return NextResponse.redirect(new URL("/sign-in", req.url));
         }
         
         if(isApiRequest && !isPublicApiRoute(req)){
-            return NextResponse.redirect(new URL("/signin", req.url));
+            return NextResponse.redirect(new URL("/sign-in", req.url));
         }
     }
 
